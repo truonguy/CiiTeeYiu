3f4d6e52da3fc72dcaba2c74807edb9d3553c7c2
Uy Trường <truonguy123tx@gmail.com>
2023-02-14 15:55:32 +0700

    upload source code

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "To NowTeeYiu",
+    introTitle: 'Nguyễn Tố Uyên',
+    introDesc: `14/2 anh bị "Ngáo" ngồi làm cái này, xem thử nhé`,
+    btnIntro: '^^Ấn vào đâyy^^',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Em cũng thế :< <33',
+    btnNo: 'Không nha, anh trai :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh? <3',
+    btnReply: 'Gửi cho CiiTeeYiu <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Ngồi mãi mới làm xong đấy :)).',
+    btnAccept: ' Ghee',
+    messLink: 'https://www.facebook.com/messages/t/100014188333536%7D' 
+}
